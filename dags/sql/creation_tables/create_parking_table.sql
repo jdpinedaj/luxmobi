a41b2ba8c58b79c1e5533a8f77fcf25f990ad7b2
Juan Pineda <juandpineda@gmail.com>
2023-11-07 05:33:11 -0500

    chore: increase postgres statement_timeout to 1 hour

--- dags/sql/creation_tables/create_parking_table.sql
+++ dags/sql/creation_tables/create_parking_table.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS {{ params.table_name }};
+-- DROP TABLE IF EXISTS {{ params.table_name }};
 CREATE TABLE IF NOT EXISTS {{ params.table_name }} (
     date DATE NOT NULL,
     hour INT NOT NULL,
